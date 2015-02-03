eafe8ec2ee927279b522355943d7225da4c96c19
Larissa Ubungen <larissaubungen@gmail.com>
2015-02-02 21:39:28 -0800

    Database folder

--- ApplicantResume.php
+++ ApplicantResume.php
@@ -185,6 +185,7 @@ if($_POST['resume'] == "Submit")
     
            <!-- Placed at the end of the document so the pages load faster --> 
 	<script src="js/jquery-1.7.2.min.js"></script> 
+	<script src="js/jquery-1.3.2.min.js"></script> 
 	<script src="js/excanvas.min.js"></script> 
 	<script src="js/chart.min.js" type="text/javascript"></script> 
 	<script src="js/bootstrap.js"></script>
