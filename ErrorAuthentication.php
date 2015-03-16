ebb060028d0d9c49f758f941d281587de315de16
Larissa Ubungen <larissaubungen@gmail.com>
2015-03-15 17:21:02 -0700

    ErrorLogIn Fix and Error Authentication
    Page Restrictions based on Employment Status

--- ErrorAuthentication.php
+++ ErrorAuthentication.php
@@ -1,3 +1,10 @@
+<?php
+	session_start();
+    if (!isset($_SESSION['ID_No'])) {
+    header('Location:login.php');
+  } 
+?>
+
 <html lang="en">
 <HEAD>
 	<meta charset="utf-8">
@@ -20,6 +27,7 @@
 		margin-left: 6em;
 	}
 	</style>
+
 </HEAD>
 <BODY>
 	<!--Navigation bar-->
@@ -47,13 +55,51 @@
 		<div class="content clearfix">		
 			<div class="alert alert-danger" role="alert">
 
-		      <div class=/"alert-link"/> <b><h3> Error!</h3> <br/>  </b>
-		       Username and Password does not Match<br/>
-		      Kindly log in your account details correctly. 
+		      <div class=/"alert-link"/> <b><h3> Authorization Error!</h3> <br/>  </b>
+		       You are not authorized to view this Page.<br/>
 		  	  </div> <br/>
 
-      	<input type="submit" class="btn btn-link" onClick="location='login.php'" value="Retry Login">
+		<?php
+		$user = $_SESSION['ID_No'];
+
+		mysql_connect("localhost", "root", "")
+				or die(mysql_error());
+				mysql_select_db("lbas_hr") 
+				or die(mysql_error());
+				
+
+		$checkStatus = mysql_query
+					   ("
+					   	SELECT E_Status
+					   	FROM person 
+					   	WHERE ID_No = '".$user."'
+							");
+		while ($row = mysql_fetch_array($checkStatus)) {
+			$E_Stat = $row["E_Status"];
+
+			if ($E_Stat == 'Employee'){
+		?>
+      		<input type="submit" class="btn btn-link" onClick="location='EmployeesPage.php'" value="Return to Home Page">
+      	<?php		
+    		}
+    		else if ($E_Stat == 'Admin') {
+    	?>
+    		<input type="submit" class="btn btn-link" onClick="location='HR_Page.php'" value="Return to Home Page">
+    	<?php	
+    		}
+    		else if ($E_Stat == 'Applicant'){
+    	?>
+    		<input type="submit" class="btn btn-link" onClick="location='ApplicantPage.php'" value="Return to Home Page">
+    	<?php
+    		}
+    		else{ //has no account in the system or E_Status
+		?>
+    			<input type="submit" class="btn btn-link" onClick="location='login.php'" value="Return to Login Page">
+		<?php
+    		}
+    		}
+		?>
     		</div>
     	</div>
-	</BODY>
+</BODY>
 </HTML>
